5825f8d7aa51321faefaf60476a311f91c0066ba
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-06-30 02:29:01 -0500

    starting local storage functions

--- script.js
+++ script.js
@@ -17,24 +17,58 @@ var description = $(`.description`);
 $(function () {
   saveBtn.on("click", function (event) {
     var time = $(this).parent().attr("id");
-    console.log(today);
+    // console.log();
     var input = $(event.target).parent().children().eq(1).val();
     
     localStorage.setItem("appointment", JSON.stringify(input));
 
   });
+  $(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
+  });
+  $(".time-div").each(function () {
+    var timeDiv = $(this).attr("id").split("-")[1];
+    
+    if (currentHour == timeDiv) {
+      $(this).addClass("present");
+      $(this).children(".description").addClass("white-text");
+    } else if (currentHour < timeDiv) {
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    } else if (currentHour > timeDiv) {
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+    console.log(currentHour);
+  });
   function scheduler () {
-    var now = moment().hour();
+    var now = moment().format("HH");
     $(".row").each(function () {
       var go = parseInt($(this).attr("id").split("hour")[1]);
       if (go < now){
         $(this).removeClass("presnt");
-        $(this).removeClass("presnt");
-        $(this).removeClass("presnt");
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      }
+          else if (go === now){
+          $(this).removeClass("past");
+          $(this).removeClass("future");
+          $(this).addClass("present");
+      }
+            else {
+            $(this).removeClass("presnt");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+            console.log(now);
+            console.log(go);
+            localStorage.setItem("appointment", JSON.stringify(scheduler));
       }
     })
   }
-
+  
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -52,7 +86,25 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+      $("#hour-9 .time-block").val(localStorage.getItem("9"));
+      $("#hour-10 .time-block").val(localStorage.getItem("10"));
+      $("#hour-11 .time-block").val(localStorage.getItem("11"));
+      $("#hour-12 .time-block").val(localStorage.getItem("12"));
+      $("#hour-13 .time-block").val(localStorage.getItem("13"));
+      $("#hour-14 .time-block").val(localStorage.getItem("14"));
+      $("#hour-15 .time-block").val(localStorage.getItem("15"));
+      $("#hour-16 .time-block").val(localStorage.getItem("16"));
+      $("#hour-17 .time-block").val(localStorage.getItem("17"));
+      localStorage.setItem("9", JSON.stringify($("#hour-9 .time-block").val));
+      localStorage.setItem("10", JSON.stringify($("#hour-10 .time-block").val));
+      localStorage.setItem("11", JSON.stringify($("#hour-11 .time-block").val));
+      localStorage.setItem("12", JSON.stringify($("#hour-12 .time-block").val));
+      localStorage.setItem("9", JSON.stringify($("#hour-1 .time-block").val));
+      localStorage.setItem("9", JSON.stringify($("#hour-2 .time-block").val));
+      localStorage.setItem("9", JSON.stringify($("#hour-3 .time-block").val));
+      localStorage.setItem("9", JSON.stringify($("#hour-4 .time-block").val));
+      localStorage.setItem("9", JSON.stringify($("#hour-5 .time-block").val));
+
   // TODO: Add code to display the current date in the header of the page.
   
 
