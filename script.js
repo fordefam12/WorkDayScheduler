7e196bdee76d89947b975362f97c714974b72973
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-06-30 01:38:18 -0500

    setting up schedule function

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var hour9 = $('#hour-9');
-var hour10 = $('#hour-10');
-var hour11 = $('#hour-11');
-var hour12 = $('#hour-12');
-var hour1 = $('#hour-1');
-var hour2 = $('#hour-2');
-var hour3 = $('#hour-3');
-var hour4 = $('#hour-4');
-var hour5 = $('#hour-5');
+var today = dayjs();
+var hour9 = $("#hour-9");
+var hour10 = $("#hour-10");
+var hour11 = $("#hour-11");
+var hour12 = $("#hour-12");
+var hour1 = $("#hour-1");
+var hour2 = $("#hour-2");
+var hour3 = $("#hour-3");
+var hour4 = $("#hour-4");
+var hour5 = $("#hour-5");
 var saveBtn = $(`.saveBtn`);
 var description = $(`.description`);
 // console.log($('.saveBtn').parent().eq(0).children(0).eq(1).val());
 $(function () {
- saveBtn.on('click', function () {
+  saveBtn.on("click", function (event) {
+    var time = $(this).parent().attr("id");
+    console.log(today);
+    var input = $(event.target).parent().children().eq(1).val();
+    
+    localStorage.setItem("appointment", JSON.stringify(input));
 
-  // console.log($('.saveBtn').parent().eq(0).children().eq(1).children(0).val);
- }) 
+  });
+  function scheduler () {
+    var now = moment().hour();
+    $(".row").each(function () {
+      var go = parseInt
+    })
+  }
 
-  var container = $(`.container-lg`);
- container.on('click', '.saveBtn', function (event) {
-  var input = $(event.target).parent().children().eq(1).val();
-  console.log(input)
-  localStorage.setItem("hour9" , JSON.stringify($(event.target).parent().children().eq(1).val()));
 
- })
- 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -45,6 +49,8 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  
+
+  $("#currentDay").text(today.format("dddd, MMMM D YYYY, h:mm:ss a"));
+  // console.log(($("#currentDay").text(today.format("dddd, MMMM D YYYY, h:mm:ss a"))));
 });
-var today = dayjs();
-$('#currentDay').text(today.format('dddd, MMMM D YYYY, h:mm:ss a'));
